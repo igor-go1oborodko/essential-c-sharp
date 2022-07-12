4d38d3cdbe96286a67c53ef7a4cd567650730ec1
Igor Goloborodko <igor.goloborodko@gmail.com>
2022-07-12 12:50:47 +0300

    add basic methods tasks solution

---
+++ basic-methods-tasks/basic-methods-tasks/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+enum SortOrder
+{
+    Ascdending,
+    Descengin
+}
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    static bool isSorted(int[] arr, bool order)
+    {
+        if (order)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
