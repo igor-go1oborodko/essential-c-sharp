b6ff5370b86ca9f6e25b121b1d2a9c8f1b53ba70
Igor Goloborodko <igor.goloborodko@gmail.com>
2022-07-15 17:27:49 +0300

    add rectangle class

--- classes-basics/classes-basics/Rectangle.cs
+++ classes-basics/classes-basics/Rectangle.cs
@@ -1,10 +1,58 @@
-﻿using System;
-namespace classes_basics
+﻿namespace classes_basics
 {
     public class Rectangle
     {
+        private double sideA;
+        private double sideB;
+
+        public Rectangle(double a, double b)
+        {
+            this.sideA = a;
+            this.sideB = b;
+        }
+
+        public Rectangle(double a)
+        {
+            this.sideA = a;
+            this.sideB = 5;
+        }
+
         public Rectangle()
         {
+            this.sideA = 4;
+            this.sideB = -3;
+        }
+
+        public double getSideA()
+        {
+            return this.sideA;
+        }
+
+        public double getSideB()
+        {
+            return this.sideB;
+        }
+
+        public double Area()
+        {
+            return this.sideA * this.sideB;
+        }
+
+        public double Perimeter()
+        {
+            return (this.sideA + this.sideB) * 2;
+        }
+
+        public bool IsSquare()
+        {
+            return this.sideA == this.sideB;
+        }
+
+        public void ReplaceSides()
+        {
+            double temp = this.sideA;
+            this.sideA = this.sideB;
+            this.sideB = temp;
         }
     }
 }
